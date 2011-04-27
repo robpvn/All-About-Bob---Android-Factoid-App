a23fdd2ef275bc59cde8de7d5ca74bc4026f565a
Robert Nordan <rpvn@robpvn.net>
2011-04-27 14:23:08 +0200

    Hopefully 1.0 version, all functionality implemented w/ anon content.

--- gen/robpvn/allaboutbob/R.java
+++ gen/robpvn/allaboutbob/R.java
@@ -8,6 +8,9 @@
 package robpvn.allaboutbob;
 
 public final class R {
+    public static final class array {
+        public static final int fun_facts=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
@@ -15,18 +18,25 @@ public final class R {
         public static final int profile=0x7f020001;
     }
     public static final class id {
-        public static final int factsAboutTextView=0x7f050002;
-        public static final int funfactTextView=0x7f050005;
-        public static final int nextFactoidButton=0x7f050004;
-        public static final int pictureImageView=0x7f050001;
-        public static final int relativeLayout1=0x7f050000;
-        public static final int timerTextView=0x7f050003;
+        public static final int didYouKnowTextView=0x7f060006;
+        public static final int factsAboutTextView=0x7f060002;
+        public static final int funfactTextView=0x7f060005;
+        public static final int nextFactoidButton=0x7f060004;
+        public static final int pictureImageView=0x7f060001;
+        public static final int relativeLayout1=0x7f060000;
+        public static final int timerTextView=0x7f060003;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int countdown_aftertext=0x7f040006;
+        public static final int countdown_beforetext=0x7f040005;
+        public static final int countdown_date=0x7f040004;
+        public static final int countdown_prepend=0x7f040007;
+        public static final int didyouknow=0x7f040003;
+        public static final int next_fact_button=0x7f040002;
+        public static final int title=0x7f040000;
     }
 }
--- src/robpvn/allaboutbob/MainWindow.java
+++ src/robpvn/allaboutbob/MainWindow.java
@@ -1,13 +1,142 @@
 package robpvn.allaboutbob;
 
-import android.app.Activity;
-import android.os.Bundle;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class MainWindow extends Activity {
-    /** Called when the activity is first created. */
+import org.apache.commons.lang.time.DurationFormatUtils;
+
+import android.app.Activity;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainWindow extends Activity implements OnClickListener {
+	
+	private String[] factList;
+	private TextView factBox;
+	private Random numGen;
+	private int currentFact;
+	private Timer countdownTimer;
+	
+	private Button nextFactoidButton;
+	
+	private Date eventDate;
+	private TextView timerText;
+	
+    /** Called when the activity is first created, sets up everything. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        numGen = new Random();
+        factList = getResources().getStringArray(R.array.fun_facts);
+        factBox = (TextView) findViewById(R.id.funfactTextView);
+        displayRandomFact();
+        
+        nextFactoidButton = (Button) findViewById(R.id.nextFactoidButton);
+        nextFactoidButton.setOnClickListener(this);
+        
+        //It may be deprecated, but I don't see equivalent functionality in the replacement.
+        eventDate = new Date(getResources().getString(R.string.countdown_date));
+        timerText = (TextView) findViewById(R.id.timerTextView);
+        this.runOnUiThread(upDateCountdown);
+        //Setting up the timer to update once a minute
+        GregorianCalendar when = new GregorianCalendar();
+        when.set(GregorianCalendar.MINUTE, when.get(GregorianCalendar.MINUTE) + 1);
+        when.set(GregorianCalendar.SECOND, 0);
+        countdownTimer = new Timer();
+        countdownTimer.schedule(new TimerTask() {
+        	@Override
+        	public void run() {
+        		onTick();
+        	}
+
+        }, when.getTime(), 60000);
+        
     }
+    
+    /** Called when the device is rotated to reinitialize the UI 
+     * (Background data and timers are still running from onCreate)*/
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+      super.onConfigurationChanged(newConfig);
+      setContentView(R.layout.main);
+      
+      factBox = (TextView) findViewById(R.id.funfactTextView);
+      factBox.setText(factList[currentFact]);
+      
+      nextFactoidButton = (Button) findViewById(R.id.nextFactoidButton);
+      nextFactoidButton.setOnClickListener(this);
+      
+      timerText = (TextView) findViewById(R.id.timerTextView);
+      this.runOnUiThread(upDateCountdown);
+      
+    }
+        
+    /**
+     * Shows a new random fact in the view box.
+     * (Actually pseudorandom because it will not display the same fact twice.)
+     */
+	private void displayRandomFact() {
+
+		int random = numGen.nextInt(factList.length);
+		if (random != currentFact) {
+			factBox.setText(factList[random]);
+			currentFact = random;
+		} else {
+			displayRandomFact();
+		}
+	}
+
+    /**
+     * Updates the countdown/up timer.
+     */
+	private Runnable upDateCountdown = new Runnable() {
+		public void run() {	
+			Date now = new Date();
+
+			
+
+			if (now.before(eventDate)) {	//In the future
+				String elapsed = DurationFormatUtils.formatPeriod(now.getTime(), eventDate.getTime(), 
+				"y \'years,\' M \'months,\' d \'days, \'H \'hours and \' m \'minutes\'");
+				
+				timerText.setText(getResources().getString(R.string.countdown_prepend)
+						+ " " + elapsed + " " 
+						+ getResources().getString(R.string.countdown_beforetext));
+			} else {						//In the past
+				String elapsed = DurationFormatUtils.formatPeriod(eventDate.getTime(), now.getTime(), 
+				"y \'years,\' M \'months,\' d \'days, \'H \'hours and \' m \'minutes\'");
+				
+				timerText.setText(getResources().getString(R.string.countdown_prepend)
+						+ " " + elapsed + " " 
+						+ getResources().getString(R.string.countdown_aftertext));
+			}
+
+
+		}
+	};
+	
+	/** Reacts to the user requesting a new fact.*/
+    public void onClick(View clicked) {
+    	
+    	if (clicked == nextFactoidButton) {
+    		displayRandomFact();
+		} 
+    	
+    }
+    
+    /** Handles ticks from the timer once a minute. */
+    protected void onTick()
+	{
+		this.runOnUiThread(upDateCountdown);
+	}
+	
 }
